1adc34fd53b26d8847961f6affbf8c6253fbb31f
Vyygir <open.source@vyygir.me>
2024-09-04 22:44:50 +0100

    refactor: move UI structs to a more relevant place

--- src/Interface.h
+++ src/Interface.h
@@ -8,58 +8,6 @@
 #include <mutex>
 #include "SDL_ttf.h"
 
-struct Dimensions {
-	int w = -1;
-	int h = -1;
-};
-
-struct Scale {
-	float x = 1.0f;
-	float y = 1.0f;
-};
-
-struct Offset {
-	int x;
-	int y;
-};
-
-enum class HorizontalAlignment {
-	Left,
-	Center,
-	Right,
-};
-
-enum class VerticalAlignment {
-	Top,
-	Center,
-	Bottom,
-};
-
-struct Alignment {
-	HorizontalAlignment x;
-	VerticalAlignment y;
-};
-
-struct UIElement {
-	SDL_Texture *texture;
-	SDL_Rect rect;
-	Offset offset;
-	bool repeatX;
-	bool repeatY;
-};
-
-struct InteractiveElement {
-	UIElement element;
-	std::function<void()> onClick;
-	std::string fontName;
-	std::string text;
-	int fontSize;
-	SDL_Color color;
-	SDL_Color hoverColor;
-	bool isHovered;
-	bool isClicked;
-};
-
 class Interface {
 public:
 	static void initialise(SDL_Renderer* renderer);
@@ -86,8 +34,6 @@ private:
 
 	SDL_Renderer *renderer;
 	std::map<std::string, std::string> fonts;
-	std::queue<UIElement> queue;
-	std::queue<InteractiveElement*> interactiveQueue;
 	SDL_Color backgroundColor = { 0, 0, 0, 255 };
 
 	void getWindowSize(int &width, int &height) const;
--- src/UI/Elements.h
+++ src/UI/Elements.h
@@ -3,6 +3,58 @@
 
 #pragma once
 
+struct Dimensions {
+	int w = -1;
+	int h = -1;
+};
+
+struct Scale {
+	float x = 1.0f;
+	float y = 1.0f;
+};
+
+struct Offset {
+	int x;
+	int y;
+};
+
+enum class HorizontalAlignment {
+	Left,
+	Center,
+	Right,
+};
+
+enum class VerticalAlignment {
+	Top,
+	Center,
+	Bottom,
+};
+
+struct Alignment {
+	HorizontalAlignment x;
+	VerticalAlignment y;
+};
+
+struct UIElement {
+	SDL_Texture *texture;
+	SDL_Rect rect;
+	Offset offset;
+	bool repeatX;
+	bool repeatY;
+};
+
+struct InteractiveElement {
+	UIElement element;
+	std::function<void()> onClick;
+	std::string fontName;
+	std::string text;
+	int fontSize;
+	SDL_Color color;
+	SDL_Color hoverColor;
+	bool isHovered;
+	bool isClicked;
+};
+
 #include "Elements/Text.h"
 #include "Elements/Image.h"
 #include "Elements/RepeatableImage.h"
